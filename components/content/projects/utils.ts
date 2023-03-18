b32bccdff59e2af44a7324cb94e0e50b6e457147
ita <cto@itaseoul.org>
2023-03-18 15:06:04 +0900

    first

--- components/content/projects/utils.ts
+++ components/content/projects/utils.ts
@@ -1,7 +1,9 @@
 export const projects = [
   {
     title: "한강, 2022.9.17 출발",
-    description: `A complete design system for a website that can be used for any website with a dark and light theme and a lot of components premade based TailwindCSS.`,
+    description: `에너지러닝은 기존 이타서울에서 진행 되었던  두런두런 캠페인을 바탕으로 
+    이타서울에서 폐 전자제품과 재활용품을 활용하여 직접 제작한 Iot (사물인터넷) v 풍력발전기를 들고 짝꿍과 함께v  달리는 러닝 프로그램입니다.
+    `,
     img: "/re100run/1.png",
     href: "/projects/ecommerce",
     preview: "https://www.ripple-ui.com/",
@@ -13,8 +15,22 @@ export const projects = [
   },
   {
     title: "한강, 2022.9.17 출발",
-    description: `A complete design system for a website that can be used for any website with a dark and light theme and a lot of components premade based TailwindCSS.`,
+    description: `본 프로젝트는 11월 한달간 5회 오프라인 프로그램진행 하였습니다.
+    총 30명의 러너가 참여하였으며 누적 거리 20km를 달성하며 풍력발전기를 통해 생산된 전기 에너지 생산량은  6000kw 생산하였습니다.
+    `,
     img: "/re100run/2.png",
+    href: "/projects/ecommerce",
+    preview: "https://www.ripple-ui.com/",
+    github: "https://github.com/Siumauricio/rippleui",
+    alt: "Projects",
+    platform: "Web",
+    stack: "TailwindCSS, Nextjs, Typescript, CSS.",
+    delay: 0.1,
+  },
+  {
+    title: "한강, 2022.9.17 출발",
+    description: `A complete design system for a website that can be used for any website with a dark and light theme and a lot of components premade based TailwindCSS.`,
+    img: "/re100run/3.png",
     href: "/projects/ecommerce",
     preview: "https://www.ripple-ui.com/",
     github: "https://github.com/Siumauricio/rippleui",
