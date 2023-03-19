55b52284073bae1e798c477a57e1d94f7d53da3f
ita <cto@itaseoul.org>
2023-03-19 15:27:19 +0900

    second_230319

--- track/store/history/history.tsx
+++ track/store/history/history.tsx
@@ -4,17 +4,17 @@ const SET_RUN_HISTORY = "history/set_run_history";
 const DELETE_RUN = "history/delete_run";
 const SET_FETCHED_HISTORY = "history/set_fetched_history";
 
-export const setRunHistory = ([key, value]) => ({
+export const setRunHistory = ([key, value]: any) => ({
 	type: SET_RUN_HISTORY,
 	payload: [key, value],
 });
 
-export const deleteRun = (payload) => ({
+export const deleteRun = (payload: any) => ({
 	type: DELETE_RUN,
 	payload,
 });
 
-export const setFetchedHistory = (payload) => ({
+export const setFetchedHistory = (payload: any) => ({
 	type: SET_FETCHED_HISTORY,
 	payload,
 });
@@ -23,14 +23,14 @@ const historyState = {
 	runs: {},
 };
 
-export function historyReducer(state = historyState, action) {
+export function historyReducer(state = historyState, action: any) {
 	switch (action.type) {
 		case SET_RUN_HISTORY:
-			return produce(state, (draft) => {
+			return produce(state, (draft: any) => {
 				draft.runs[action.payload[0]] = action.payload[1];
 			});
 		case DELETE_RUN:
-			return produce(state, (draft) => {
+			return produce(state, (draft: any) => {
 				delete draft.runs[action.payload];
 			});
 		case SET_FETCHED_HISTORY:
